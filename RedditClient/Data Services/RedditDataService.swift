e1375cdf3ca269cdd0d24c14b44323206a0686e9
Tischuk, Christopher <ctischuk@edmunds.com>
2018-04-04 07:37:09 -0700

    Added data model and data service class

---
+++ RedditClient/Data Services/RedditDataService.swift
@@ -0,0 +1,60 @@
+//
+//  RedditDataService.swift
+//  RedditClient
+//
+//  Created by Tischuk, Christopher on 4/4/18.
+//  Copyright © 2018 Tischuk, Christopher. All rights reserved.
+//
+
+import Foundation
+
+class RedditDataService {
+    private let urlSession = URLSession(configuration: .default)
+    private let baseUrl = "https://www.reddit.com/r/popular/top/.json?"
+    
+    func getPosts(completion: @escaping (String?, [RedditPost]?) -> Void) {
+        guard let url = URL(string: baseUrl) else {
+            completion(nil, nil)
+            return
+        }
+        
+        let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
+            guard let data = data,
+                let response = response as? HTTPURLResponse,
+                let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+                let json = jsonObject as? [String: Any],
+                error == nil,
+                response.statusCode == 200 else {
+                    completion(nil, nil)
+                    return
+            }
+            let redditPostResponse = RedditPostResponse(json: json)
+            completion(redditPostResponse?.after, redditPostResponse?.posts)
+        }
+        
+        dataTask.resume()
+    }
+}
+
+fileprivate struct RedditPostResponse {
+    var after: String?
+    var posts: [RedditPost]?
+    
+    init?(json: [String: Any]) {
+        guard let dataJson = json["data"] as? [String: Any],
+            let childrenJson = dataJson["children"] as? [[String: Any]] else { return nil }
+        
+        self.after = dataJson["after"] as? String
+        
+        var redditPosts = [RedditPost]()
+        
+        for childJson in childrenJson {
+            guard let childDataJson = childJson["data"] as? [String: Any] else { continue }
+            
+            if let redditPost = RedditPost(json: childDataJson) {
+                redditPosts.append(redditPost)
+            }
+        }
+        posts = redditPosts
+    }
+}
---
+++ RedditClient/Models/RedditPost.swift
@@ -0,0 +1,42 @@
+//
+//  RedditPost.swift
+//  RedditClient
+//
+//  Created by Tischuk, Christopher on 4/4/18.
+//  Copyright © 2018 Tischuk, Christopher. All rights reserved.
+//
+
+import Foundation
+
+enum postType {
+    case image
+    case standard
+}
+
+struct RedditPost {
+    var title: String
+    var author: String?
+    var dateCreated: Date
+    var commentsCount: Int
+    var imageUrl: String?
+    var type: postType
+    
+    init?(json: [String: Any]) {
+        guard let title = json["title"] as? String,
+            let dateValue = json["created_utc"] as? Double,
+            let commentsCount = json["num_comments"] as? Int else { return nil }
+        
+        self.title = title
+        self.author = json["author"] as? String
+        self.dateCreated = Date(timeIntervalSince1970: dateValue)
+        self.commentsCount = commentsCount
+        if let imageUrl = json["thumbnail"] as? String,
+            imageUrl.lowercased() != "default" {
+            self.imageUrl = imageUrl
+            self.type = .image
+        }
+        else {
+            self.type = .standard
+        }
+    }
+}
