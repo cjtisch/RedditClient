d5a0ebed522ed36ec4f30089e92a6867d503c963
Tischuk, Christopher <ctischuk@edmunds.com>
2018-04-04 08:23:32 -0700

    Added image saving and state restoration

--- RedditClient/View Controllers/ImageDetailViewController.swift
+++ RedditClient/View Controllers/ImageDetailViewController.swift
@@ -12,6 +12,7 @@ class ImageDetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageUrl: String?
+    private let imageRestorationKey = "imageUrl"
     private lazy var imageService = ImageService()
     
     override func viewDidLoad() {
@@ -33,11 +34,48 @@ class ImageDetailViewController: UIViewController {
             imageView.image = image
         }
         else {
-            imageService.downloadImage(forURL: imageUrl, completion: { [weak self] (image) in
+            imageService.downloadImage(forURL: imageUrl) { [weak self] (image) in
                 DispatchQueue.main.async {
                     self?.imageView.image = image
                 }
-            })
+            }
         }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func saveImage(_ sender: UIBarButtonItem) {
+        guard let image = imageView.image else { return }
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeMutableRawPointer) {
+        if error != nil {
+            let alertController = UIAlertController(title: "Error", message: "Could not save photo at this time.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+        else {
+            let alertController = UIAlertController(title: "Saved!", message: nil, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - State Restoration
+    
+    override func encodeRestorableState(with coder: NSCoder) {
+        if let imageUrl = imageUrl {
+            coder.encode(imageUrl, forKey: imageRestorationKey)
+        }
+        
+        super.encodeRestorableState(with: coder)
+    }
+    
+    override func decodeRestorableState(with coder: NSCoder) {
+        imageUrl = coder.decodeObject(forKey: imageRestorationKey) as? String
+        
+        super.decodeRestorableState(with: coder)
     }
 }
